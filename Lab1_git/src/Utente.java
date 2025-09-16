75e07bc74f640a421c6329babc7ed32a0ad27268
sergiosantos <sergiosantos@localhost>
2025-09-16 11:45:02 +0100

    adição de gets e sets classe utente-2

--- Lab1_git/src/Utente.java
+++ Lab1_git/src/Utente.java
@@ -19,4 +19,60 @@ public class Utente {
 		this.altura = altura;
 		this.peso = peso;
 	}
+
+	/**
+	 * @return the idade
+	 */
+	public int getIdade() {
+		return idade;
+	}
+
+	/**
+	 * @param idade the idade to set
+	 */
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	/**
+	 * @return the altura
+	 */
+	public float getAltura() {
+		return altura;
+	}
+
+	/**
+	 * @param altura the altura to set
+	 */
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	/**
+	 * @return the peso
+	 */
+	public int getPeso() {
+		return peso;
+	}
+
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @return the genero
+	 */
+	public String getGenero() {
+		return genero;
+	}
 }
