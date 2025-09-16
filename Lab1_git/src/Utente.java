e74dfee9c157cfe2e419ad8d2175e1447630ea82
sergiosantos <sergiosantos@localhost>
2025-09-16 11:35:39 +0100

    primeira versao da classe Utente

---
+++ Lab1_git/src/Utente.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Utente {
+	private String nome;
+	private String genero;
+	private int idade;
+	private float altura;
+	private int peso;
+	
+	public Utente(String nome, String genero, int idade, float altura, int peso) {
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+	}
+}
